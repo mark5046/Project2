8aed03a887fa2c6b8b3f370339c55aa000e56e42
Mark Iudice <markiudice@Marks-MacBook-Pro.local>
2018-04-25 14:49:19 -0400

    adding table hopefully

--- schema.sql
+++ schema.sql
@@ -3,4 +3,13 @@ CREATE DATABASE instacode;
 
 USE instacode;
 
-CREATE TABLE 
+CREATE TABLE profile (
+    id INT NOT NULL AUTO_INCREMENT,
+    user_name VARCHAR(30) NOT NULL,
+    password VARCHAR(30) NOT NULL,
+    category VARCHAR(30) NOT NULL,
+    comments TEXT NULL,
+    likes INT NULL,
+    PRIMARY KEY (id)
+    );
+    
