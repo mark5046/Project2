040fbe89c14df3ae3766ef47bed9ef0058d65d97
Seungha Kong <kseha1080@gmail.com>
2018-04-28 15:17:27 -0400

    kong update

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -26,7 +26,7 @@ $(document).ready(function () {
       password: password
     }).then(function(data) {
       window.location.replace(data);
-      // If there's an error, log the error
+     
     }).catch(function(err) {
       console.log(err);
     });
