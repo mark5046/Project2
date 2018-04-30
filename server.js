cb83fecdf9fa4ddb5306e06aadda73f611d009b7
Mark Iudice <markiudice@Marks-MacBook-Pro.local>
2018-04-30 15:37:55 -0400

    m

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 
-db.sequelize.sync({force: true}).then(function() {
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });
